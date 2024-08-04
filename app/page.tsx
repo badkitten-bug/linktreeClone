2b41001541ab9db326ed5fce7bb66a7c0d34166e
Steve Gomez <104510289+badkitten-bug@users.noreply.github.com>
2024-08-03 19:20:29 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,57 @@
-import ProfileName from '../app/components/ProfileName';
-import LinkButton from '../app/components/LinkButton';
-import ProfilePicture from './components/ProfilePicture';
+import LinkButton from "@/components/LinkButton";
+import ProfileName from "@/components/ProfileName";
+import ProfilePicture from "@/components/ProfilePicture";
+import SocialLinks from "@/components/SocialLinks";
+
 const Home: React.FC = () => {
   const links = [
-    { label: "Claim your linktree", url: "https://www.google.com" },
-    { label: "Business Blog", url: "https://www.google.com" },
-    { label: "Forbes Interview", url: "https://www.google.com" },
+    {
+      label: "Pagina web para cuidar casas",
+      url: "https://www.google.com",
+      iconUrl: "/github.png",
+    },
+    {
+      label: "My Portfolio",
+      url: "https://www.google.com",
+      iconUrl: "/github.png",
+    },
+    {
+      label: "Business Blog",
+      url: "https://www.google.com",
+      iconUrl: "/github.png",
+    },
+    {
+      label: "Vuelos Baratos",
+      url: "https://www.google.com",
+      iconUrl: "/github.png",
+    },
+    {
+      label: "Instagram",
+      url: "https://www.google.com",
+      iconUrl: "/github.png",
+    },
   ];
 
   return (
-    <main className="flex flex-col items-center p-8 sm:p-16 md:p-24 bg-gray-100 min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/img/background.png')" }}>
+    <main
+      className="flex flex-col items-center p-8 sm:p-16 md:p-24 bg-gray-100 min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/img/background.png')" }}
+    >
       <ProfilePicture src="/dinosaur.png" alt="Profile Picture" />
-      <ProfileName name="Brookerty" subname="Your place to learn what's new and discover what's good at Linktree."/>
+      <ProfileName name="@Steve" subname="HOUSITTING POR EL MUNDO." />
       <div className="w-full sm:w-2/3 lg:w-1/2">
         {links.map((link, idx) => (
-          <LinkButton key={idx} label={link.label} url={link.url} />
+          <LinkButton
+            key={idx}
+            label={link.label}
+            url={link.url}
+            iconUrl={link.iconUrl}
+          />
         ))}
       </div>
-      <div className="flex justify-center mt-10 space-x-6">
-        {["Facebook", "Github", "Linkedin"].map((platform, idx) => (
-          <div key={idx} className="text-blue-600 hover:text-blue-800 transition-colors cursor-pointer">
-            {platform}
-          </div>
-        ))}
-      </div>
+      <SocialLinks />
     </main>
   );
-}
+};
 
 export default Home;
---
+++ components/LinkButton.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+interface LinkButtonProps {
+  label: string;
+  url: string;
+  iconUrl?: string;
+  customClass?: string;
+}
+
+const LinkButton: React.FC<LinkButtonProps> = ({
+  label,
+  url,
+  iconUrl,
+  customClass,
+}) => (
+  <div className={`w-full mt-4 ${customClass}`}>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="w-full block"
+    >
+      <button className="bg-slate-900 text-white py-4 px-6 w-full rounded-full hover:bg-slate-800 transition-colors shadow-md text-lg;">
+        {iconUrl && (
+          <Image
+            src={iconUrl}
+            alt="icon"
+            width={24}
+            height={24}
+            className="mr-4"
+          />
+        )}
+        {label}
+      </button>
+    </a>
+  </div>
+);
+
+export default LinkButton;
